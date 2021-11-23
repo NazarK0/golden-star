5a8036556f47ab3097c66ebd68932133afe5cf5b
NazarK0 <staticman999@gmail.com>
2021-11-23 13:06:05 +0200

    some ui changes

--- admin-ui/src/index.js
+++ admin-ui/src/index.js
@@ -6,15 +6,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 
 // layouts
-
 import Admin from "layouts/Admin.js";
 import Auth from "layouts/Auth.js";
-
-// views without layouts
-
-import Landing from "views/Landing.js";
-import Profile from "views/Profile.js";
-import Index from "views/Index.js";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -22,12 +15,8 @@ ReactDOM.render(
       {/* add routes with layouts */}
       <Route path="/admin" component={Admin} />
       <Route path="/auth" component={Auth} />
-      {/* add routes without layouts */}
-      <Route path="/landing" exact component={Landing} />
-      <Route path="/profile" exact component={Profile} />
-      <Route path="/" exact component={Index} />
       {/* add redirect for first page */}
-      <Redirect from="*" to="/" />
+      <Redirect from="*" to="/admin" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
--- app-ui/src/layouts/PageLayout/PageLayout.js
+++ app-ui/src/layouts/PageLayout/PageLayout.js
@@ -1,15 +1,14 @@
 /*eslint-disable*/
 import React from "react";
-import { Link } from "react-router-dom";
 
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
-import s from './HomePage.module.css'
-import HomePage from "views/GalleryPage/GalleryPage";
-import AboutPage from "views/AboutPage/AboutPage";
-import GalleryPage from "views/GalleryPage/GalleryPage";
-import NewsPage from "views/NewsPage/NewsPage";
-import ContactsPage from "views/ContactsPage/ContactsPage";
+import s from './PageLayout.module.css'
+import HomePage from "views/GalleryPage";
+import AboutPage from "views/AboutPage";
+import GalleryPage from "views/GalleryPage";
+import NewsPage from "views/NewsPage";
+import ContactsPage from "views/ContactsPage";
 
 export default function PageLayout() {
   return (
