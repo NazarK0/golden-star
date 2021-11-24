3667d492dd0198c92af203807b009db160b47854
NazarK0 <staticman999@gmail.com>
2021-11-24 11:54:10 +0200

    about page

--- app-ui/src/views/ContactsPage/ContactsPage.js
+++ app-ui/src/views/ContactsPage/ContactsPage.js
@@ -5,7 +5,10 @@ import s from './ContactsPage.module.css'
 export default function ContactsPage() {
   return (
     <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
-      
+      <div>
+        <h2>Зв'яжіться з нами</h2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d635.400426248593!2d30.5438723708993!3d50.4298920814164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf0c044dd735%3A0x77940fa521b6fd28!2z0JLRltC50YHRjNC60L7QstC40Lkg0ZbQvdGB0YLQuNGC0YPRgiDRgtC10LvQtdC60L7QvNGD0L3RltC60LDRhtGW0Lkg0YLQsCDRltC90YTQvtGA0LzQsNGC0LjQt9Cw0YbRltGXINGW0LzQtdC90ZYg0JPQtdGA0L7Rl9CyINCa0YDRg9GC!5e0!3m2!1suk!2sua!4v1637746230355!5m2!1suk!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
     </section>
   );
 }
